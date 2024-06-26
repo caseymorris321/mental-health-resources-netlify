99daa3b29cf65aef449090ed4f00d507cf120e9a
Casey Morris <morrisacasey@gmail.com>
2024-06-26 00:19:05 -0400

    update logout button

--- frontend/src/components/LogoutButton.js
+++ frontend/src/components/LogoutButton.js
@@ -1,13 +1,18 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = () => {
-    const { logout, isAuthenticated } = useAuth0();
-    
+    const { logoutWithRedirect, isAuthenticated } = useAuth0();
+
     return (
         isAuthenticated && (
-            <button onClick={() => logout()} className="btn btn-outline-danger">Sign Out</button>
+            <button
+                onClick={() => logoutWithRedirect({ returnTo: window.location.origin })}
+                className="btn btn-outline-danger"
+            >
+                Sign Out
+            </button>
         )
-    )
-}
+    );
+};
 
 export default LogoutButton;
