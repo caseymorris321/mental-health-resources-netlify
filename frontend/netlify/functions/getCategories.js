4e9f61db508a8bdd02c60918efa43a5e61d34d68
Casey Morris <morrisacasey@gmail.com>
2024-06-25 22:17:32 -0400

    update db connection

--- frontend/netlify/functions/getCategories.js
+++ frontend/netlify/functions/getCategories.js
@@ -3,7 +3,7 @@ const { Category } = require('./models/resourceModel');
 
 exports.handler = async (event, context) => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
--- frontend/netlify/functions/updateResource.js
+++ frontend/netlify/functions/updateResource.js
@@ -4,7 +4,7 @@ const authMiddleware = require('./middleware/requireAuth');
 
 const handler = async (event, context) => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
