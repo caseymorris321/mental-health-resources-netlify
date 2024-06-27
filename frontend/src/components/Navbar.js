a8a7ebff33ef6c5bc53fd088f242e64c91edb742
Casey Morris <morrisacasey@gmail.com>
2024-06-27 09:46:15 -0400

    update navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 import LogoutButton from './LogoutButton';
 import Profile from './Profile';
 
