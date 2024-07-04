412f4642c973d6f036fb6dc5f5b8d1e235b18c55
Casey Morris <morrisacasey@gmail.com>
2024-07-04 12:49:28 -0400

    change bullets to dashes

--- frontend/src/components/QuickLinks.js
+++ frontend/src/components/QuickLinks.js
@@ -7,7 +7,7 @@ const QuickLinks = ({ categories, onQuickLinkClick }) => (
       <ul className="list-unstyled ps-3">
         {categories.slice(0, 5).map(category => (
           <li key={category._id} className="d-flex align-items-center py-1">
-            <span className="me-2">•</span>
+            <span className="me-2">-</span>
             <button
               onClick={() => onQuickLinkClick(category._id)}
               className="btn btn-link p-0 m-0 text-decoration-none text-start"
