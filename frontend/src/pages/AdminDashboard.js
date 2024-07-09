c8bcaf7c9f172111e8726c940c4e426286a63e8f
Casey Morris <morrisacasey@gmail.com>
2024-07-09 16:11:26 -0400

    update drag n drop

--- frontend/src/pages/AdminDashboard.js
+++ frontend/src/pages/AdminDashboard.js
@@ -406,75 +406,75 @@ const AdminDashboard = () => {
     }
   };
 
-  const handleMoveCategory = async (categoryId, direction) => {
-    try {
-      const token = await getAccessTokenSilently();
-      const response = await fetch(fetchUrl(isProduction ? `moveCategory/${categoryId}/${direction}` : `categories/${categoryId}/move/${direction}`), {
-        method: 'PUT',
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (response.ok) {
-        const updatedCategories = await response.json();
-        // console.log('Updated categories:', updatedCategories);
-        setCategories(updatedCategories);
-      } else {
-        console.error('Failed to move category');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
+//   const handleMoveCategory = async (categoryId, direction) => {
+//     try {
+//       const token = await getAccessTokenSilently();
+//       const response = await fetch(fetchUrl(isProduction ? `moveCategory/${categoryId}/${direction}` : `categories/${categoryId}/move/${direction}`), {
+//         method: 'PUT',
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//         },
+//       });
+//       if (response.ok) {
+//         const updatedCategories = await response.json();
+//         // console.log('Updated categories:', updatedCategories);
+//         setCategories(updatedCategories);
+//       } else {
+//         console.error('Failed to move category');
+//       }
+//     } catch (error) {
+//       console.error('Error:', error);
+//     }
+//   };
 
-  const handleMoveSubCategory = async (subCategoryId, direction) => {
-    try {
-      const token = await getAccessTokenSilently();
-      const response = await fetch(fetchUrl(isProduction ? `moveSubCategory/${subCategoryId}/${direction}` : `subcategories/${subCategoryId}/move/${direction}`), {
-        method: 'PUT',
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (response.ok) {
-        const updatedSubCategories = await response.json();
-        setSubCategories(updatedSubCategories);
-        // We might need to fetch all subcategories again to ensure the state is fully updated
-        fetchSubCategories();
-      } else {
-        console.error('Failed to move subcategory');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
+//   const handleMoveSubCategory = async (subCategoryId, direction) => {
+//     try {
+//       const token = await getAccessTokenSilently();
+//       const response = await fetch(fetchUrl(isProduction ? `moveSubCategory/${subCategoryId}/${direction}` : `subcategories/${subCategoryId}/move/${direction}`), {
+//         method: 'PUT',
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//         },
+//       });
+//       if (response.ok) {
+//         const updatedSubCategories = await response.json();
+//         setSubCategories(updatedSubCategories);
+//         // We might need to fetch all subcategories again to ensure the state is fully updated
+//         fetchSubCategories();
+//       } else {
+//         console.error('Failed to move subcategory');
+//       }
+//     } catch (error) {
+//       console.error('Error:', error);
+//     }
+//   };
 
- const handleMoveResource = async (resourceId, direction) => {
-  try {
-    const token = await getAccessTokenSilently();
-    const response = await fetch(fetchUrl(isProduction ? `moveResource/${resourceId}/${direction}` : `${resourceId}/move/${direction}`), {
-      method: 'PUT',
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    if (response.ok) {
-      const updatedResource = await response.json();
-      setResources(prevResources => {
-        const newResources = prevResources.filter(r => r._id !== resourceId);
-        const insertIndex = direction === 'up' 
-          ? newResources.findIndex(r => r.category === updatedResource.category && r.subCategory === updatedResource.subCategory)
-          : newResources.findIndex(r => r.category === updatedResource.category && r.subCategory === updatedResource.subCategory) + 1;
-        newResources.splice(insertIndex, 0, updatedResource);
-        return newResources;
-      });
-    } else {
-      console.error('Failed to move resource');
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  }
-};
+//  const handleMoveResource = async (resourceId, direction) => {
+//   try {
+//     const token = await getAccessTokenSilently();
+//     const response = await fetch(fetchUrl(isProduction ? `moveResource/${resourceId}/${direction}` : `${resourceId}/move/${direction}`), {
+//       method: 'PUT',
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+//     if (response.ok) {
+//       const updatedResource = await response.json();
+//       setResources(prevResources => {
+//         const newResources = prevResources.filter(r => r._id !== resourceId);
+//         const insertIndex = direction === 'up' 
+//           ? newResources.findIndex(r => r.category === updatedResource.category && r.subCategory === updatedResource.subCategory)
+//           : newResources.findIndex(r => r.category === updatedResource.category && r.subCategory === updatedResource.subCategory) + 1;
+//         newResources.splice(insertIndex, 0, updatedResource);
+//         return newResources;
+//       });
+//     } else {
+//       console.error('Failed to move resource');
+//     }
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
+// };
 
 const onDragEnd = async (result) => {
   if (!result.destination) {
