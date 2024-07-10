ea95693154cc073576d87f17e44328ab8fa7d219
Casey Morris <morrisacasey@gmail.com>
2024-07-10 01:17:26 -0400

    update netlify.toml

--- frontend/src/pages/AdminDashboard.js
+++ frontend/src/pages/AdminDashboard.js
@@ -212,7 +212,7 @@ const AdminDashboard = () => {
     }
     try {
       const token = await getAccessTokenSilently();
-      const response = await fetch(fetchUrl('undoDeleteResource'), {
+      const response = await fetch(`/.netlify/functions/undoDeleteResource`, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
@@ -233,6 +233,7 @@ const AdminDashboard = () => {
       console.error('Error:', error);
     }
   };
+  
   
   
   
