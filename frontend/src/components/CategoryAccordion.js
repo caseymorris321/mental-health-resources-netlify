a2da44515a978526cca5f41cafda61c484739c48
Casey Morris <morrisacasey@gmail.com>
2024-07-12 12:36:51 -0400

    update colors

--- frontend/src/components/CategoryAccordion.js
+++ frontend/src/components/CategoryAccordion.js
@@ -7,13 +7,13 @@ const CategoryAccordion = ({ id, category, subCategories, resources, columns, is
       <div
         className="card-header"
         onClick={onToggle}
-        style={{ 
+        style={{
           cursor: 'pointer',
-          background: '#007BFF', // Primary blue background
+          background: 'white', // Primary blue background
           border: '1px solid #0056b3' // Darker blue border
         }}
       >
-        <h2 className='text-center text-white mb-0'>{category.name}</h2>
+        <h2 className='text-center mb-0' style={{ color: '#333333' }}>{category.name}</h2>
       </div>
       {isExpanded && (
         <div className="card-body" style={{ backgroundColor: '#F8F9FA' }}>
@@ -28,7 +28,8 @@ const CategoryAccordion = ({ id, category, subCategories, resources, columns, is
 
             return (
               <div key={subCategory._id} className="mb-4 p-3" id={`subcategory-${subCategory.name.replace(/\s+/g, '-').toLowerCase()}`} style={{ backgroundColor: '#FFFFFF', borderRadius: '0.25rem', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
-                <h3 className='text-center text-primary mb-3'>{subCategory.name}</h3>
+                <h3 className='text-center' style={{ color: '#7F98B2' }}>{subCategory.name}</h3>
+
                 <ResourceTable
                   columns={columns}
                   data={subCategoryResources}
--- frontend/src/components/WelcomeScreen.js
+++ frontend/src/components/WelcomeScreen.js
@@ -4,7 +4,7 @@ const WelcomeScreen = ({ onClose }) => (
   <div className="welcome-screen">
     <h2>Welcome to Mental Health Resources</h2>
     <p>This site provides a comprehensive list of mental health resources. Use the search bar to find specific resources, or browse through our categories.</p>
-    <button className="btn btn-primary" onClick={onClose}>Get Started</button>
+    <button className="btn" style={{ backgroundColor: '#5CB85C', color: 'white' }} onClick={onClose}>Get Started</button>
   </div>
 );
 
