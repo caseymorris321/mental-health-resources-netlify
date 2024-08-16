db704a6e6bb75f8b2e57ade1a24e91def523632e
Casey Morris <morrisacasey@gmail.com>
2024-08-16 14:33:29 -0400

    fix move subcategory

--- frontend/netlify/functions/moveSubCategory.js
+++ frontend/netlify/functions/moveSubCategory.js
@@ -14,50 +14,53 @@ exports.handler = async (event, context) => {
     const { subCategoryId, newIndex, newCategory } = JSON.parse(event.body);
 
     const subCategory = await SubCategory.findById(subCategoryId);
-    if (!subCategory) {
-      return { statusCode: 404, body: JSON.stringify({ message: 'Subcategory not found' }) };
+    if (!subCategory || subCategory.isDeleted) {
+      return { statusCode: 404, body: JSON.stringify({ message: 'Subcategory not found or is deleted' }) };
     }
 
     const oldCategory = subCategory.category;
     const oldIndex = subCategory.order;
 
+    // Get all active subcategories in the old and new categories
+    const oldCategorySubCategories = await SubCategory.find({ category: oldCategory, isDeleted: false }).sort('order');
+    const newCategorySubCategories = oldCategory === newCategory 
+      ? oldCategorySubCategories 
+      : await SubCategory.find({ category: newCategory, isDeleted: false }).sort('order');
+
     if (oldCategory === newCategory) {
       // Moving within the same category
-      if (newIndex > oldIndex) {
-        await SubCategory.updateMany(
-          { category: newCategory, order: { $gt: oldIndex, $lte: newIndex } },
-          { $inc: { order: -1 } }
-        );
-      } else if (newIndex < oldIndex) {
-        await SubCategory.updateMany(
-          { category: newCategory, order: { $gte: newIndex, $lt: oldIndex } },
-          { $inc: { order: 1 } }
-        );
+      const updatedSubCategories = oldCategorySubCategories.filter(sc => sc._id.toString() !== subCategoryId);
+      updatedSubCategories.splice(newIndex, 0, subCategory);
+      
+      for (let i = 0; i < updatedSubCategories.length; i++) {
+        updatedSubCategories[i].order = i;
+        await updatedSubCategories[i].save();
       }
     } else {
       // Moving to a different category
-      await SubCategory.updateMany(
-        { category: oldCategory, order: { $gt: oldIndex } },
-        { $inc: { order: -1 } }
-      );
-      await SubCategory.updateMany(
-        { category: newCategory, order: { $gte: newIndex } },
-        { $inc: { order: 1 } }
-      );
-    }
+      oldCategorySubCategories.splice(oldIndex, 1);
+      newCategorySubCategories.splice(newIndex, 0, subCategory);
 
-    subCategory.order = newIndex;
-    subCategory.category = newCategory;
-    await subCategory.save();
+      for (let i = 0; i < oldCategorySubCategories.length; i++) {
+        oldCategorySubCategories[i].order = i;
+        await oldCategorySubCategories[i].save();
+      }
+
+      for (let i = 0; i < newCategorySubCategories.length; i++) {
+        newCategorySubCategories[i].order = i;
+        newCategorySubCategories[i].category = newCategory;
+        await newCategorySubCategories[i].save();
+      }
+    }
 
     // Update associated resources
     await Resource.updateMany(
-      { subCategory: subCategory.name, category: oldCategory },
+      { subCategory: subCategory.name, category: oldCategory, isDeleted: false },
       { $set: { category: newCategory } }
     );
 
-    const updatedSubCategories = await SubCategory.find().sort('category order');
-    const updatedResources = await Resource.find().sort('category subCategory order');
+    const updatedSubCategories = await SubCategory.find({ isDeleted: false }).sort('category order');
+    const updatedResources = await Resource.find({ isDeleted: false }).sort('category subCategory order');
 
     return {
       statusCode: 200,
