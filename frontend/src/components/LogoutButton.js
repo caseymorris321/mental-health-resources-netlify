220f1146e940e75d9d880141b1f45b66716e4bdc
Casey Morris <morrisacasey@gmail.com>
2024-08-16 18:22:21 -0400

    fix logout

--- frontend/src/components/LogoutButton.js
+++ frontend/src/components/LogoutButton.js
@@ -1,10 +1,13 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from 'react-router-dom';
 
 const LogoutButton = () => {
     const { logout, isAuthenticated } = useAuth0();
-    
+    const navigate = useNavigate();
+
     const handleLogout = () => {
-        logout({
+        localStorage.setItem('redirectAfterLogout', '/');
+        logout({ 
             logoutParams: {
                 returnTo: window.location.origin
             }
